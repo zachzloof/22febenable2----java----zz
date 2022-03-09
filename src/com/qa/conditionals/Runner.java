a59b9d24a1fe0019d0ba74599394a5376c011634
Zach Zloof <zachzloof@hotmail.com>
2022-03-09 18:30:46 +0000

    Third commit

--- src/com/qa/conditionals/Runner.java
+++ src/com/qa/conditionals/Runner.java
@@ -11,18 +11,17 @@ public class Runner {
 //        } else {
 //            System.out.println("The light is turned off");
 //        }
-		
-		
+
 //		BlackJack game1 = new BlackJack();
 //		System.out.println(game1.blackJackMethod(15, 17));
-		
+
 //		UniqueSum game2 = new UniqueSum();
 //		System.out.println(game2.uniqueSumMethod(3, 3 ,3));
-				
+
 		Tax game3 = new Tax();
-		double wage = 30000;
-		System.out.println(game3.methodOne(wage));
-		System.out.println(game3.methodTwo(wage));
-    }
-		
+		//System.out.println(game3.methodOne());
+		System.out.println(game3.methodThree(50000));
+		//System.out.println(game3.takeHome());
+	}
+
 }
--- src/com/qa/conditionals/Tax.java
+++ src/com/qa/conditionals/Tax.java
@@ -8,7 +8,6 @@ public class Tax {
 		if (wage < 0) {
 			wage = wage * -1;
 		}
-		
 		if (wage <=14999) {
 			multiplier = 1;
 			message = "You are in the 0% tax bracket";
@@ -26,10 +25,36 @@ public class Tax {
 			message = "You are in the 25% tax bracket";
 		}
 		return message;
-			
+	}
+	public double methodTwo(Double wage) {
+		double tax = 0;
+		if (wage < 0) {
+			wage = wage * -1;
+		}
+		if (wage <=14999) {
+			multiplier = 1;
+			tax = 0;
+		} else if (wage <= 19999) {
+			multiplier = .9;
+			tax = wage * .1;
+		} else if (wage <= 29999) {
+			multiplier = .85;
+			tax = wage * .15;
+		} else if (wage <= 44999) {
+			multiplier = .8;
+			tax = wage * .2;
+		} else {
+			multiplier = .75;
+			tax = wage * .25;
+		}
+		return tax;
 	}
 	
-	public String methodTwo(double wage) {
+	public String methodThree(double wage) {
+		return this.methodOne(wage) + "\n" + this.methodTwo(wage);
+	}
+	
+	public String takeHome(double wage) {
 		double salaryAfterTax = wage * multiplier;
 		return "You will take home: £" + salaryAfterTax;
 	}
