4a7fd8d095fb5c5618914f1a4212313314a15dc6
Zach Zloof <zachzloof@hotmail.com>
2022-03-09 20:47:05 +0000

    Third commit (FizzBuzz)

---
+++ src/com/qa/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,18 @@
+package com.qa.fizzbuzz;
+
+public class FizzBuzz {
+	public String methodOne(int input) {
+		String message;
+		if (input % 3 == 0 && input % 5 == 0) {
+			message = "FizzBuzz";
+		} else if (input % 3 == 0) {
+			message = "Fizz";
+		} else if (input % 5 == 0) {
+			message = "Buzz";
+		} else {
+			message = input + "";
+		}
+		return message;
+	}
+
+}
---
+++ src/com/qa/fizzbuzz/Runner.java
@@ -0,0 +1,11 @@
+package com.qa.fizzbuzz;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			FizzBuzz object = new FizzBuzz();
+			System.out.println(object.methodOne(15));
+	}
+
+}
